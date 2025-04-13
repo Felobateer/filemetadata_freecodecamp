0727f72cf9840217baa55d6d243923859ebef321
Felobateer <felobateersamy01@gmail.com>
2025-04-13 11:28:31 +0200

    submit answer

--- index.js
+++ index.js
@@ -11,8 +11,22 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const file = req.file;
+  if (!file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  const fileInfo = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  };
 
+  res.json(fileInfo);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
